5780a802d4d91091f5b082c37863ce791ed9013f
Plaris-algedi <karchalmohammed@gmail.com>
2023-08-14 16:37:10 -0700

    Fix an attribute's name and change tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -16,51 +16,48 @@ from time import sleep
 class TestPlace(unittest.TestCase):
     """Test Cases for the Place class."""
 
-    def setUp(self):
-        """Sets up test resources"""
-        self.u1 = Place()
-        self.u1.city_id = str(uuid.uuid4())
-        self.u1.user_id = str(uuid.uuid4())
-        self.u1.name = "a place"
-        self.u1.description = "a Beach"
-        self.u1.number_rooms = 5
-        self.u1.number_bathrooms = 3
-        self.u1.max_guest = 10
-        self.u1.price_by_night = 100
-        self.u1.latitude = 40.7128
-        self.u1.longitude = -74.0060
-        self.u1.amenity_ids = ["amenity_id1", "amenity_id2"]
-
-    def tearDown(self):
-        """Tears down test resources"""
-        self.u1 = None
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
     def test_IsInstanceOf(self):
         """Test instance"""
-        self.assertEqual(str(type(self.u1)), "<class 'models.place.Place'>")
-        self.assertIsInstance(self.u1, Place)
-        self.assertTrue(issubclass(type(self.u1), BaseModel))
+        place = Place()
+        self.assertEqual(str(type(place)), "<class 'models.place.Place'>")
+        self.assertIsInstance(place, Place)
+        self.assertTrue(issubclass(type(place), BaseModel))
 
-    def test_attributes(self):
+    def test_attributes_existence_and_type(self):
         """Test attributes existence"""
-        u1_dict = self.u1.__dict__
-        self.assertIn('id', u1_dict)
-        self.assertIn('created_at', u1_dict)
-        self.assertIn('updated_at', u1_dict)
-        self.assertIn('city_id', u1_dict)
-        self.assertIn('user_id', u1_dict)
-        self.assertIn('name', u1_dict)
-        self.assertIn('description', u1_dict)
-        self.assertIn('number_rooms', u1_dict)
-        self.assertIn('number_bathrooms', u1_dict)
-        self.assertIn('max_guest', u1_dict)
-        self.assertIn('price_by_night', u1_dict)
-        self.assertIn('latitude', u1_dict)
-        self.assertIn('longitude', u1_dict)
-        self.assertIn('amenity_ids', u1_dict)
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertTrue(type(place.city_id) == str)
+
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertTrue(type(place.user_id) == str)
+
+        self.assertTrue(hasattr(place, "name"))
+        self.assertTrue(type(place.name) == str)
+
+        self.assertTrue(hasattr(place, "description"))
+        self.assertTrue(type(place.description) == str)
+
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertTrue(type(place.number_rooms) == int)
+
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertTrue(type(place.number_bathrooms) == int)
+
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertTrue(type(place.max_guest) == int)
+
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertTrue(type(place.price_by_night) == int)
+
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertTrue(type(place.latitude) == float)
+
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertTrue(type(place.longitude) == float)
+
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertTrue(type(place.amenity_ids) == list)
 
 
 if __name__ == '__main__':
